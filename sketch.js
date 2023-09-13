904081142c9271971e2d5eefe7f4168e99d7b2e0
FIRST_NAME LAST_NAMEKate <kec22@fsu.edu>
2023-09-13 11:32:32 -0400

    ball bouncing

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+let x = 100;
+let speed = 1; 
+let diameter = 40;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(125);
+  ellipse(x, height/2, 40)
+  x += speed;
+  if(x >= 400) {
+    speed = speed * -1;
+  }
+
+  
 }
+
+//framecount makes a number grow by 1 every frame
+// xPosition = xPosition + 1; controls speed and starting point
+// x += speed is the same as x = x + speed
