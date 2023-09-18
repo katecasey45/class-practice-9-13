1dc30b1de358e740be3c886113fdee7b4b96a73d
FIRST_NAME LAST_NAMEKate <kec22@fsu.edu>
2023-09-18 14:57:13 -0400

    Ball stops on click

--- sketch.js
+++ sketch.js
@@ -1,21 +1,29 @@
 let x = 100;
-let speed = 1; 
+let speed = 2; 
 let diameter = 40;
 
 function setup() {
   createCanvas(400, 400);
+
 }
 
+
 function draw() {
-  background(125);
+  background(245,222,179);
   ellipse(x, height/2, 40)
   x += speed;
   if(x >= 400 || x<=0) {
     speed = speed * -1;
   }
 
-  
 }
+noloop()
+function mouseClicked() {
+ speed = 0
+}
+
+
+
 
 //framecount makes a number grow by 1 every frame
 // xPosition = xPosition + 1; controls speed and starting point
