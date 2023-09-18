8975837977f714568517095b4257f4588e67c0b8
FIRST_NAME LAST_NAMEKate <kec22@fsu.edu>
2023-09-18 17:30:53 -0400

    one pausing ball and one constantly moving ball

--- sketch.js
+++ sketch.js
@@ -1,26 +1,50 @@
-let x = 100;
-let speed = 2; 
+let x1 = 100;
+let speed1 = 2; 
 let diameter = 40;
+let pause = 0
+let x2 = 100
+let speed2 = 2
 
 function setup() {
   createCanvas(400, 400);
-
+loop()
 }
-
 
 function draw() {
   background(245,222,179);
-  ellipse(x, height/2, 40)
-  x += speed;
-  if(x >= 400 || x<=0) {
-    speed = speed * -1;
+  ellipse(x1, height/2, 40)
+  if (!pause){
+  x1 += speed1;
+  if(x1 >= 400 || x1<=0) {
+    speed1 = speed1 * -1;
   }
+}
+
+  ellipse(x2, height/3, 40)
+  x2 += speed2;
+  if(x2 >= 400 || x2<=0) {
+    speed2 = speed2 * -1;
+  }
+  }
+  
+
+
+function mouseClicked(){
+  if (pause) {
+    speed1 = 2;
+  } else { 
+    speed1 = 0
+  }
+pause = !pause
 
 }
-noloop()
-function mouseClicked() {
- speed = 0
-}
+
+
+
+
+
+
+
 
 
 
